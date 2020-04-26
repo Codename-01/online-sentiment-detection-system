895b22dfb292b4dcb80ad9b7153c0d3d2015f77a
Codename-01 <1013297146@qq.com>
2020-04-27 01:01:50 +0800

    add movie model

--- OnlineSentimentDetection/models/run_classifier_sentiment.sh
+++ OnlineSentimentDetection/models/run_classifier_sentiment.sh
@@ -4,8 +4,8 @@
 # @Last Modified by:   bo.shi
 # @Last Modified time: 2020-01-01 11:46:07
 
-TASK_NAME="movie"
-MODEL_NAME="albert_tiny"
+TASK_NAME="restaurant"
+MODEL_NAME="albert_restaurant"
 CURRENT_DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 export CUDA_VISIBLE_DEVICES="0"
 export BERT_PRETRAINED_MODELS_DIR=$CURRENT_DIR/prev_trained_model
@@ -51,8 +51,8 @@ if [ $# == 0 ]; then
       --max_seq_length=128 \
       --per_gpu_train_batch_size=16 \
       --per_gpu_eval_batch_size=16 \
-      --learning_rate=2e-5 \
-      --num_train_epochs=3.0 \
+      --learning_rate=1e-3 \
+      --num_train_epochs=1 \
       --logging_steps=3335 \
       --save_steps=3335 \
       --output_dir=$CURRENT_DIR/${TASK_NAME}_output/ \
@@ -62,7 +62,7 @@ elif [ $1 == "predict" ]; then
     echo "Start predict..."
     python3 run_classifier.py \
       --model_type=albert \
-      --model_name_or_path=$CURRENT_DIR/${TASK_NAME}_output/albert/ \
+      --model_name_or_path=$MODEL_NAME \
       --task_name=$TASK_NAME \
       --do_predict \
       --do_lower_case \
